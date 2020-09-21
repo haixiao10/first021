4fff5aa0fa6bc7a05967c5adc671d784d304e97f
haixaio <dj09872@gmail.com>
2020-09-21 16:23:56 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<style>
+    body {
+        font-family:微軟正黑體;
+    }
+</style>
+<title>梁依玲0903</title>
+</head>
+<body>
+<h2>梁依玲0903 PHP練習記錄</h2>
+<hr>
+<?php include "menu.php"; ?>
+<hr>
+<?php include "footer.php"; ?>
+</body>
+</html>
